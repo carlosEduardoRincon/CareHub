2c17b2bb670776582567ad3f778a9e43b6dae0e4
carlosEduardoRincon <carloscaca147@gmail.com>
2025-11-03 18:02:02 -0300

    feat: add project structure

---
+++ src/test/java/com/care/hub/CareHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.care.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
